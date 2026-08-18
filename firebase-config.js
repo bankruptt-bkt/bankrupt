a89a96db1332a30daab984a0221fbddbfc799a07
bankruptt-bkt <cryptogambler18@gmail.com>
2026-08-18 18:14:09 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQT7gM7JxE26bFq061VvZauWkEGjyHPWM",
+  authDomain: "bankrupt-9068b.firebaseapp.com",
+  databaseURL: "https://bankrupt-9068b-default-rtdb.firebaseio.com",
+  projectId: "bankrupt-9068b",
+  storageBucket: "bankrupt-9068b.firebasestorage.app",
+  messagingSenderId: "961644576786",
+  appId: "1:961644576786:web:65eff34df07a18067458cb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
